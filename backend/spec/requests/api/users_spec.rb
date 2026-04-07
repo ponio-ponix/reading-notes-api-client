46d1f7cb9d9d855f066cf621b3cb53326560fd74
ponio-ponix <kaikaezentei2015@gmail.com>
2026-04-07 14:05:24 +0900

    Test users create success and validation failures

---
+++ backend/spec/requests/api/users_spec.rb
@@ -0,0 +1,102 @@
+require 'rails_helper'
+
+RSpec.describe "Api::Users", type: :request do
+  let!(:example_user) do
+    User.create!(
+      email: "abab@example.com",
+      password: "password"
+    )
+  end
+
+
+  describe "POST /api/users" do
+
+    context "正常系" do
+      it "emailとパスワードが登録できること" do
+        post "/api/users",
+          params: { email: "note-spec-#{SecureRandom.hex(4)}@example.com", password: "password"} ,
+          as: :json
+          # puts response.body
+          expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "異常系" do
+
+      context "必須" do
+        it "emailが空文字" do
+   
+          post "/api/users",
+                params: { email: "", password: "password" },
+                as: :json
+          
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+    
+        it "パスワードが空文字" do
+          post "/api/users",
+              params: { email: "note-spec-#{SecureRandom.hex(4)}@example.com", password: ""},
+              as: :json
+          
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+    
+       
+        it "emailがnil" do
+       
+          post "/api/users",
+                params: { email: nil, password: "password" },
+                as: :json
+    
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+    
+        it "パスワードがnil" do
+       
+          post "/api/users",
+                params: { email: "note-spec-#{SecureRandom.hex(4)}@example.com", password: nil },
+                as: :json
+          
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+        it "emailのキー自体がない" do
+        
+          post "/api/users",
+                params: { password: "password" },
+                as: :json
+    
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+    
+        it "パスワードのキー自体がない" do
+        
+          post "/api/users",
+                params: { email: "note-spec-#{SecureRandom.hex(4)}@example.com" },
+                as: :json
+          
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+      context "重複" do
+        it "すでに登録してあるemailで再度登録しようとした場合" do
+      
+          post "/api/users",
+                params: { email: "abab@example.com", password: "password"},
+                as: :json
+          
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context "形式不正" do
+        it "emailの形式不正" do
+          post "/api/users",
+            params: { email: "note-spec-#{SecureRandom.hex(4)}example.com", password: "password"} ,
+            as: :json
+    
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+  end
+end
