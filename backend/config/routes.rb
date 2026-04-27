b3f231bea512cc42627470995b51a35fc0db0fe5
ponio-ponix <kaikaezentei2015@gmail.com>
2026-04-27 17:13:17 +0900

    fix(notes): use scoped book in destroy specs

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,14 +4,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :books, only: [:index, :create] do
-      resources :notes, only: [:create]
+      resources :notes, only: [:create, :destroy]
       resources :notes_search, only: [:index], path: "notes_search"
 
       post "notes/bulk", to: "notes_bulk#create"
     end
 
-    resources :notes, only: [:destroy]
-    
     namespace :auth do
       resource :session, only: [:create, :destroy]
     end
