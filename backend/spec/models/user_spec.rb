76d2a43f45bc886f77ab6e5ac4cb4f55574fab11
ponio-ponix <kaikaezentei2015@gmail.com>
2026-08-03 14:49:44 +0900

    Add User model validation specs

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "メールアドレスがない場合は無効である" do
+      user = described_class.new(
+        name: "Test User",
+        email: nil,
+        password: "password"
+      )
+
+      expect(user).not_to be_valid
+    end
+    it "パスワードがない場合は無効である" do
+      user = described_class.new(
+        name: "Test User",
+        email: "test@example.com",
+        password: nil
+      )
+    
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to be_present
+    end
+  end
 end
